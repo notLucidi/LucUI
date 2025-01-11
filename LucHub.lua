5e75087d8dbab62a419cd1fde87ed41d172e839b
notLucid <103009610+notLucidi@users.noreply.github.com>
2025-01-11 05:38:23 -0800

    Update LucHub.lua

--- LucHub.lua
+++ LucHub.lua
@@ -1,1 +1,11 @@
-local lucUI = load(MakeRequest("https://raw.githubusercontent.com/notLucidi/LucUI/refs/heads/main/LucUI.lua", "GET", "").content)()
+local lucUI = load(makeRequest("https://raw.githubusercontent.com/notLucidi/LucUI/refs/heads/main/LucUI.lua", "GET", "").content)()
+
+local Window = lucUI:Window({
+    Label = "Label Example",
+    BackgroundColor = color("backgroundDark"),
+    BorderColor = color("grayDark"),
+    Icon = 1234,
+    ExitButton = true
+})
+
+sendVariant({[0] = "OnDialogRequest", [1] = Window})
