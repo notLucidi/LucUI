41563f004403983b8ee78e212ef89eefca850704
notLucid <103009610+notLucidi@users.noreply.github.com>
2025-01-11 06:08:46 -0800

    Update LucUI.lua

--- LucUI.lua
+++ LucUI.lua
@@ -1,5 +1,39 @@
 local libary = {}
 
+--// Colors //--
+local Colors = {
+    backgroundDark = {25, 0, 25, 25},
+    backgroundLight = {240, 0, 240, 240},
+    textDark = {10, 0, 10, 10},
+    textLight = {255, 0, 255, 255},
+    borderDark = {50, 0, 50, 50},
+    borderLight = {200, 0, 200, 200},
+    primary = {52, 0, 152, 219},
+    primaryHover = {41, 0, 128, 185},
+    accent = {46, 0, 204, 113},
+    danger = {231, 0, 76, 60},
+    secondary = {155, 0, 89, 182},
+    warning = {241, 0, 196, 15},
+    info = {52, 0, 73, 94},
+    success = {39, 0, 174, 96},
+    grayLight = {220, 0, 220, 220},
+    gray = {128, 0, 128, 128},
+    grayDark = {64, 0, 64, 64},
+    highlight = {243, 0, 156, 18},
+    selection = {142, 0, 68, 173},
+    disabled = {189, 0, 195, 199},
+}
+
+function color(color_name)
+    local color = Colors[color_name]
+    if color then
+        local useColor = table.concat(color, ",")
+        return useColor
+    else
+        return error("Colors ".. color_name .." Not Found")
+    end
+end
+
 -- Constructor untuk membuat dialog baru
 function libary:Window(properties)
     local dialog = {}
