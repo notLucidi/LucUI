81233b45dd4f0e0fa92c10f8c4dee2125a108862
notLucid <103009610+notLucidi@users.noreply.github.com>
2025-01-12 01:48:25 -0800

    Update LucHub.lua

--- LucHub.lua
+++ LucHub.lua
@@ -1,15 +1,41 @@
 local LucUI = load(makeRequest("https://raw.githubusercontent.com/notLucidi/LucUI/refs/heads/main/LucUI.lua", "GET", "").content)()
 
-local dialog1 = LucUI:Window({
-    Label = "Formulir Pengguna",
-    BackgroundColor = "backgroundDark",
-    BorderColor = "grayDark",
+-- Create a window
+local Window = LucUI:Window({
+    Label = "Example Window",
+    BackgroundColor = "backgroundDarkSolid",
+	BorderColor = "dangerSolid",
     Icon = 7188,
-    ExitButton = true
+    ExitButton = false
 })
 
-dialog1:addTextbox("LucUI")
+-- Add a tab to the window
+local Tab = Window:MakeTab({
+    Id = "example_tab",
+    Icon = "interface/large/gacha_banner.rttex",
+    Size = "228,92",
+    Frame = "0,0",
+    Width = 0.14,
+    MinWidth = 60,
+    Callback  = function()
+		logToConsole("Callback Called!")
+	end
+})
 
-local dialogString1 = dialog1:build()
-sendVariant({[0] = "OnDialogRequest", [1] = dialogString1})
+local Tab2 = Window:MakeTab({
+    Id = "example_tab1",
+    Icon = "interface/large/gacha_banner.rttex",
+    Size = "228,92",
+    Frame = "0,1",
+    Width = 0.14,
+    MinWidth = 60
+})
 
+local image = Window:addImage({
+	Id = "example_image",
+    Image = "interface/large/galaxy_banner.rttex"
+})
+
+local dialogString = Window:build()
+
+sendVariant({[0] = "OnDialogRequest", [1] = dialogString})
